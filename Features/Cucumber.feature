7c70ec04387e2426fadd1b4950178b1c8ea584be
Adam Ruth <adam.ruth01@gmail.com>
2024-01-25 16:00:49 +0100

    Update on docker,cucumber,controller test

--- Features/Cucumber.feature
+++ Features/Cucumber.feature
@@ -1,22 +1,53 @@
 Feature: Weather api
+#This scenario is for function (Get) allWeathers() in controller ( localhost:8080/weather )
+  Scenario: The client's purpose is to receive all the weather information, (in this case all the weathers objects in the database)
 
-  Scenario: The client will view weather data for a specific city
+    Given The client in this case postman will use port localhost:8080 to access the weather information.
 
-  Scenario: The client will view weather data for all cities in specific country
+    When The client in this case "postman" will use (Get) port localhost:8080/weather
+
+    Then localhost:8080/weather will receive all existing weathers and display them for the client.
 
 
-  Scenario: “API should have endpoints to fetch weather of different countries and cities.
-  These should be fetched from a text file or database on the server.
-  The following endpoints are required:
-  GET: /api/{country}/cities - show all cities in the country
-  GET: /api/weather/{city_id} - show the weather (eg “24c SUNNY” in the selected city)”
+#This scenario is for function (Get) getByCountry() in controller ( localhost:8080/weather/{country}/cities)
+  Scenario: The client will view weather data for a specific Country and the given country will show all cities in the country
 
     Given The client in this case the "postman" will use port localhost:8080 to access the weather information.
 
-    When The client will write the name in the in (country) column! Use localhost:8080/weather/Country/cities
-
-    When  The client will write the (city_id) in the in  column! localhost:8080/weather/(city_id)
+    When The client will write the name in the in {country} column! Use (Get) localhost:8080/weather/{Country}/cities
 
     Then localhost:8080/weather/{Country}/cities to access all the citys in the given country.
 
-    Then  localhost:8080/weather/{city_id} to access the city you are looking for.
+
+
+  Scenario: The client will view weather data for all cities in specific country
+#This scenario is for function (Get) getCityWeathers()  for given city_id in controller ( localhost:8080/weather/{city_id} )
+    Given The client in this case the "postman" will use port localhost:8080 to access the weather information.
+
+    When The client will write the {city_id} in the in  column!Use (Get) localhost:8080/weather/{city_id}
+
+    Then localhost:8080/weather/{city_id} to access the city you are looking for.
+
+
+
+    Scenario: The client will view weather data for all the tempratures that is greater then the given number
+      #This scenario is for function (GET) getTemprature() if temprature is greater the function will sort the given objects receive the matching weathers
+      Given The client in this case "postman" will use port localhost:8080 to access the weather information.
+
+      When The client will write the {temprature} in the colum! Use (Get) localhost:8080/weather/temp/{temp}
+
+      Then localhost:8080/weather/temp/{temp} to access the tempraures  and where they are located
+
+      Scenario: The client will view weather data for all the tempratures that is greater then the given number
+        #This scenario is for function (GET) getByHumidity() if humidity is greater the function will sort the given objects and receive the matching weathers
+      Given The client in this case "postman" will use port localhost:8080 to access the weather information.
+
+      When When The client will write the {humidity} in the colum! Use (Get) localhost:8080/weather/humidity/{humidity}
+
+      Then  localhost:8080/weather/humidity/{humidity} to access the humidity you are looking for and where the humidity are located
+
+
+
+
+
+
--- src/test/java/com/example/spring_mysql_api/WeatherControllerTests.java
+++ src/test/java/com/example/spring_mysql_api/WeatherControllerTests.java
@@ -3,17 +3,23 @@ package com.example.spring_mysql_api;
 import com.example.spring_mysql_api.controllers.WeatherController;
 import com.example.spring_mysql_api.models.Weather;
 import com.example.spring_mysql_api.services.WeatherService;
+import jdk.jfr.ContentType;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import static net.bytebuddy.matcher.ElementMatchers.hasDescriptor;
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -40,11 +46,41 @@ public class WeatherControllerTests {
 
     private static Iterable<Weather> getMockWeathers() {
         Weather[] weathers = {
-                new Weather("Test weather 1", "Stockholm", "The temprature is low", 5, 10, 75, "Feels like -1 celsius"),
-                new Weather("Test weather 2", "Halmstad", "The temprature is low", 15, 15, 10, "Feels like -1 celsius"),
-                new Weather("Test weather 3", "Krakow", "The temprature is low", 25, 14, 30, "Feels like -1 celsius"),
-                new Weather("Test weather 4", "New York", "The temprature is low", 55, 1, 100, "Feels like -1 celsius")
+                new Weather("Sweden", "Stockholm", "The temprature is low", 5, 10, 75, "Feels like -1 celsius"),
+                new Weather("Sweden", "Halmstad", "The temprature is low", 15, 15, 10, "Feels like -1 celsius"),
+                new Weather("Poland", "Krakow", "The temprature is low", 25, 14, 30, "Feels like -1 celsius"),
+                new Weather("Usa", "New York", "The temprature is low", 55, 1, 100, "Feels like -1 celsius")
         };
         return Arrays.stream(weathers).toList();
+    }
+    private static Iterable<Weather> getMockWeathers1(String country) {
+        Iterable<Weather> all= getMockWeathers();
+        ArrayList<Weather> result = new ArrayList<>();
+        for (Weather obj: all) {
+            if (obj.getCountry().equalsIgnoreCase(country))
+                result.add(obj);
+        }
+        return result;
+    }
+    private static Iterable<Weather> getMockWeathers11Old() {
+        Weather[] weathers = {
+                new Weather("Sweden", "Stockholm", "The temprature is low", 5, 10, 75, "Feels like -1 celsius"),
+                new Weather("Sweden", "Halmstad", "The temprature is low", 15, 15, 10, "Feels like -1 celsius"),
+                new Weather("Poland", "Krakow", "The temprature is low", 25, 14, 30, "Feels like -1 celsius"),
+                new Weather("Usa", "New York", "The temprature is low", 55, 1, 100, "Feels like -1 celsius")
+        };
+        return Arrays.stream(weathers).toList();
+    }
+
+    @Test
+    public void getByCountry_ShouldReturnListOfCountrys() throws Exception {
+        when(service.getByCountry("Sweden")).thenReturn(getMockWeathers1("Sweden")).getMock();
+
+            mockMvc.perform(MockMvcRequestBuilders.get("/weather/Sweden/cities"))
+                    .andExpect(status().isOk())
+
+                    .andExpect(jsonPath("$", hasSize(2)));
+                   //.andExpect((ResultMatcher) jsonPath("$.city", is("Halmstad")));
+
     }
 }
